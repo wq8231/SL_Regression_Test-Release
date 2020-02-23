8cfc212768f079522d1b42fc5eb5ccd361e6570f
wq <64439772@qq.com>
2020-02-24 01:42:04 +0800

    路径

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     localtime = time.localtime(time.time())
     now = str(localtime.tm_year) + '.' + str(localtime.tm_mon) + '.' + str(localtime.tm_mday) + '.' + str(
         localtime.tm_hour) + '.' + str(localtime.tm_min)
-    fp = open('/SL_Test_Report(%s).html' % now, 'wb')
+    fp = open('/var/lib/jenkins/workspace/SL_Regression_Test(Release)/SL_Test_Report(%s).html' % now, 'wb')
     runner = HTMLTestRunner.HTMLTestRunner(
         stream=fp,
         title='{ SL_Test_Report }',
