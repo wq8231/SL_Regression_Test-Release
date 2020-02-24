2c8b024e8895717affe3a7e7f79ea4ac8051b3f9
wq <64439772@qq.com>
2020-02-24 22:42:13 +0800

    把发送邮件换个地方

--- common.py
+++ common.py
@@ -1,7 +1,10 @@
 import requests
 import hashlib
 import re
-
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class CommonClass():
     def getwebtoken(self, name, psw):  # 获取web端token
@@ -27,4 +30,29 @@ class CommonClass():
         md5.update(psw.encode('utf-8'))
         md5psw = md5.hexdigest()
         return md5psw
+    def sendemail(self,file):
+        print("开始发送邮件")
+        smtpserver = "smtp.qq.com"  # 发件服务器
+        port = 465  # 端口
+        sender = "64439772@qq.com"  # 发送端
+        password = "pcsxctftwxkobgcb"
+        sendto = "64439772@qq.com"
+        title = "test-report"
+        message = MIMEMultipart()
+        username = "wangqian"
+        message['From'] = username
+        message['To'] = sendto
+        message['Subject'] = Header(title, 'utf-8')
+        message.attach(MIMEText('测试报告', 'plain', 'utf-8'))
+        att3 = MIMEText(
+            open(file, 'rb').read(),
+            'base64', 'utf-8')
+        att3["Content-Type"] = 'application/octet-stream'
+        att3["Content-Disposition"] = 'attachment; filename="report_test.html"'
+        message.attach(att3)
+        smtpObj = smtplib.SMTP_SSL(host=smtpserver, port=port)  # 注意：如果遇到发送失败的情况（提示远程主机拒接连接），这里要使用SMTP_SSL方法
+        smtpObj.login(sender, password)
+        smtpObj.sendmail(sender, sendto, message.as_string())
+        print("邮件发送成功！！！")
+        smtpObj.quit()
 
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 import time
 import unittest
-# from HTMLTestRunner import HTMLTestRunner
 import HTMLTestRunner
-import smtplib
-from email.header import Header
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from common import CommonClass
 
 
 def suite():
     start_dir = "/var/lib/jenkins/workspace/SL_Regression_Test(Release)"
-    top = ".\SL_Regression_Test(Release)"
     print(start_dir)
     suite = unittest.defaultTestLoader.discover(start_dir=start_dir, pattern='*test.py', top_level_dir=None)
     return suite
@@ -22,6 +17,7 @@ if __name__ == '__main__':
     now = str(localtime.tm_year) + '.' + str(localtime.tm_mon) + '.' + str(localtime.tm_mday) + '.' + str(
         localtime.tm_hour) + '.' + str(localtime.tm_min)
     fp = open('/var/lib/jenkins/workspace/SL_Regression_Test(Release)/SL_Test_Report-%s.html' % now, 'wb')
+    file='/var/lib/jenkins/workspace/SL_Regression_Test(Release)/SL_Test_Report-%s.html' % now
     runner = HTMLTestRunner.HTMLTestRunner(
         stream=fp,
         title='{ SL_Test_Report }',
@@ -31,29 +27,6 @@ if __name__ == '__main__':
     suite = suite()
     runner.run(suite)
     fp.close()
-
     time.sleep(1)
-    print("开始发送邮件")
-    smtpserver = "smtp.qq.com"  # 发件服务器
-    port = 465  # 端口
-    sender = "64439772@qq.com"  # 发送端
-    password = "pcsxctftwxkobgcb"
-    sendto = "64439772@qq.com"
-    title = "test-report"
-    message = MIMEMultipart()
-    username = "wangqian"
-    message['From'] = username
-    message['To'] = sendto
-    message['Subject'] = Header(title, 'utf-8')
-    message.attach(MIMEText('测试报告', 'plain', 'utf-8'))
-    att3 = MIMEText(
-        open('/var/lib/jenkins/workspace/SL_Regression_Test(Release)/SL_Test_Report(%s).html' % now, 'rb').read(),
-        'base64', 'utf-8')
-    att3["Content-Type"] = 'application/octet-stream'
-    att3["Content-Disposition"] = 'attachment; filename="report_test.html"'
-    message.attach(att3)
-    smtpObj = smtplib.SMTP_SSL(host=smtpserver, port=port)  # 注意：如果遇到发送失败的情况（提示远程主机拒接连接），这里要使用SMTP_SSL方法
-    smtpObj.login(sender, password)
-    smtpObj.sendmail(sender, sendto, message.as_string())
-    print("邮件发送成功！！！")
-    smtpObj.quit()
+    CommonClass().sendemail(file)
+
